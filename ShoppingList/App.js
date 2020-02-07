fcbffec8499c4ee8f2c5e37ac8dc9ae9070be002
Bhasanth <slakkaraju@ufll.edu>
2020-02-07 12:41:20 -0500

    Adds list items, add and delete items functionality

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
+import {uuid} from 'uuidv4';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 const App = () => {
+
+  const [items,setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+    {id: uuid(), text: 'Cookies'},
+  ]);
+
+  const deleteItem = (id) => {
+    setItems(prevItems =>{
+      return prevItems.filter(item => item.id != id);
+    });
+  }
+
+  const addItem = (text) => {
+    if(!text){
+      Alert.alert('Error', 'Please enter an item to add', {text: 'Ok'});
+    } else {
+      setItems(prevItems => {
+        return [{id: uuid(), text}, ...prevItems];
+      })
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>HelloWorld!!</Text>
-      <Image 
-      source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}} 
-      style={styles.img}
+      <Header title='Shopping List' />
+      <AddItem addItem={addItem}/>
+      <FlatList
+        data={items}
+        renderItem={({item}) => <ListItem item={item} deleteItem={deleteItem}/>}
       />
     </View>
   );
@@ -15,18 +44,7 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex:1, 
-    justifyContent: 'center', 
-    alignItems: 'center'
-  },
-  text: {
-    color: 'darkslateblue', 
-    fontSize: 30
-  },
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 100 / 2
+    flex:1,
   }
 });
 
